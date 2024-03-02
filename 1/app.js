fa0e45b4856d9a6a30a44aebdad3da35039145db
Chinara <151962289+ChinaraHatamli@users.noreply.github.com>
2024-03-02 19:48:59 +0100

    first commit

---
+++ 1/app.js
@@ -0,0 +1,57 @@
+//var a = 7;
+// console.log (a);
+// case sensitive
+
+// var a = 8;
+// console.log (a);
+// a = 10;
+// console.log (a);
+
+
+// let a = 10;
+// console.log (a);
+// a = 15;
+// console.log (a);
+
+
+// const y = 55;
+// console.log (y);
+
+
+// let a = 10;
+// let b = 2;
+
+// console.log (a+b);
+// console.log (a-b);
+// console.log (a*b);
+// console.log (a/b);
+// console.log (a%b); 
+// console.log (a**b);
+
+// let a = "Hello";
+// let b = "World";
+// console.log (a+ " " +b);
+
+// let a = "Hello";
+// let b = 10;
+// let c = 5;
+// console.log (a+ " " + (b+c));
+// console.log (typeof 10);
+// console.log (typeof a);
+// console.log (typeof c);
+
+
+// alert ("Mesaj ugurla silindi.");
+
+// confirm ("Mesaj silinsin?");
+
+// prompt ("Adini yaz");
+
+// let name = prompt ("Adini yaz");
+// console.log ("Salam" + " " + name);
+
+// let result = 0.1 + 0.2;
+// console.log(Math.round (result*10)/10); 
+
+
+
